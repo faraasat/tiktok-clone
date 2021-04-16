27fdfb8e5965c3225df5326638e9c0d93aa71bbf
Farasat Ali <farasataliazeemi1@gmail.com>
2021-04-16 16:52:47 +0500

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB6c00OZvcBX1nmvvlfmcoDSWMH09JXRrQ",
+  authDomain: "tiktok-clone-786.firebaseapp.com",
+  projectId: "tiktok-clone-786",
+  storageBucket: "tiktok-clone-786.appspot.com",
+  messagingSenderId: "850981367544",
+  appId: "1:850981367544:web:fe67898eb5535c6557150d",
+};
+
+export {};
